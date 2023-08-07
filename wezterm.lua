bb14a8afd052f8eb02fe1a11c98e9b088be09211
Aaron Dill <117116764+aarondill@users.noreply.github.com>
2023-08-07 12:51:16 -0500

    Improve pr.yml to only run on non-local prs
    * test formatting :)
    
    * chore: linting with Stylua
    
    * update pr.yml to only run on this repo
    
    * comment if
    
    * reintroduce if statment
    
    * only do pr workflow when repo is *not* local repo
    
    * make the condition generic
    
    ---------
    
    Co-authored-by: aarondill <aarondill@users.noreply.github.com>

--- wezterm.lua
+++ wezterm.lua
@@ -1,14 +1,26 @@
 return require("plugin")({
+
   "theme",
+
   "update",
+
   "term",
+
   "close",
+
   "window",
+
   "text",
+
   "history",
+
   "ctrl_click",
+
   "env",
+
   "toggle-transparency",
+
   "toggle-ligature",
+
   "toggle-fullscreen",
 })
