b7e53cf1258998d2b574dc17f506ad0bf0b98092
aarondill <aaronsacks2006@gmail.com>
2023-08-14 12:52:30 -0500

    define font fallbacks

--- plugins/text.lua
+++ plugins/text.lua
@@ -1,3 +1,4 @@
+local wezterm = require("wezterm")
 return {
   -- *THIS* is a word. (default)
   selection_word_boundary = " \t\n{}[]()\"'`",
@@ -14,4 +15,13 @@ return {
   font_size = 8.5,
   -- You too! (default 14.0)
   command_palette_font_size = 9.5,
+
+  font = wezterm.font_with_fallback({
+    "JetBrains Mono", -- <built-in>, BuiltIn
+    -- /usr/share/fonts/truetype/noto/NotoColorEmoji.ttf, FontConfig
+    -- Assumed to have Emoji Presentation
+    -- Pixel sizes: [128]
+    "Noto Color Emoji",
+    "Symbols Nerd Font Mono", -- <built-in>, BuiltIn
+  }),
 }
