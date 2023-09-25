d30fa3f3f3ecabfa925023801b4503c7dd80f200
aarondill <aaronsacks2006@gmail.com>
2023-09-24 23:13:49 -0500

    more reasonable initial window size

--- plugins/window.lua
+++ plugins/window.lua
@@ -1,7 +1,7 @@
 return {
   -- Change default window size (default 80, 24)
-  initial_cols = 80,
-  initial_rows = 25,
+  initial_cols = 148,
+  initial_rows = 33,
   -- Show more when i shrink font-size (default - true)
   adjust_window_size_when_changing_font_size = false,
 
