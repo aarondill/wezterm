4bc832be564e0c16ed6c4b863136f3adf54981fd
aarondill <aarondill@users.noreply.github.com>
2023-09-25 04:10:57 +0000

    chore: linting with Stylua

--- plugins/wayland.lua
+++ plugins/wayland.lua
@@ -1,6 +1,4 @@
-local util=require('util')
-local croslist="/etc/apt/sources.list.d/cros.list"
+local util = require("util")
+local croslist = "/etc/apt/sources.list.d/cros.list"
 
-if util.file_exists(croslist) then
-  return {enable_wayland=false}
-end
+if util.file_exists(croslist) then return { enable_wayland = false } end
