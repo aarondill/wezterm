73db253f9c11854ff84783f0e8650b6fc2141515
aarondill <aaronsacks2006@gmail.com>
2023-10-23 01:12:40 -0500

    abuse wezterm.glob for file_exists check

--- util/file_exists.lua
+++ util/file_exists.lua
@@ -1,10 +1,15 @@
----checks if a file exists on the filesystem (is readable to current user)
----@param name string path to the file (relative to CWD)
----@return boolean file is readable
+local wezterm = require("wezterm")
+local function escape(file) ---@param file string
+  return file:gsub("[{*\\?]", "\\%1")
+end
+---checks if a file exists on the filesystem
+---Abuses wezterm.glob.
+---@param name string? path to the file (relative to CWD)
+---@return boolean exists
 local function file_exists(name)
-  local f = io.open(name, "r")
-  if f then f:close() end
-  return f ~= nil
+  if not name then return false end
+  local res = wezterm.glob(escape(name)) ---@type string[]
+  return res[1] and true or false
 end
 
 return file_exists
