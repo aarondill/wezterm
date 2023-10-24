e21286bd507475c883b4d13a282fa8f1319b567c
aarondill <aaronsacks2006@gmail.com>
2023-10-24 01:32:14 -0500

    use wezterm.to_path if defined
    see: https://github.com/wez/wezterm/pull/4343

--- util/file_exists.lua
+++ util/file_exists.lua
@@ -8,6 +8,9 @@ end
 ---@return boolean exists
 local function file_exists(name)
   if not name then return false end
+  if wezterm.to_path then -- Check for wezterm.to_path from https://github.com/wez/wezterm/pull/4343
+    return wezterm.to_path(name):try_exists()
+  end
   local res = wezterm.glob(escape(name)) ---@type string[]
   return res[1] == name
 end
