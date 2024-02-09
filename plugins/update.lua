ed88434767de16e87d6dc0514ee7d7c1dfeeed22
aarondill <aaronsacks2006@gmail.com>
2024-02-08 21:06:40 -0600

    fix: Config::show_update_window is deprecated: this option no longer does anything

--- plugins/update.lua
+++ plugins/update.lua
@@ -7,7 +7,5 @@ return function()
     check_for_updates = true,
     -- Check three times a day (default 86400 - once a day)
     check_for_updates_interval_seconds = math.floor((24 * 60 * 60) / 3),
-    -- Show me the updates! (default)
-    show_update_window = true,
   }
 end
